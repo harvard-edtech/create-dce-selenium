f2a2ab8128c3cee5986dd6d604ca4ca396b333dd
Gabe Abrams <gabeabrams@gmail.com>
2019-05-21 11:09:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+// Import helpers
+const print = require('./helpers/print');
+const copyTo = require('./helpers/copyTo');
+const getPackageJSON = require('./helpers/getPackageJSON');
+const writePackageJSON = require('./helpers/writePackageJSON');
+
+/* eslint-disable no-console */
+
+// Prep command executor
+const exec = (command) => {
+  return execSync(command, { stdio: 'inherit' });
+};
+
+const currDir = process.env.PWD;
+
+print.title('Initializing DCE Selenium Testing');
+
+console.log('\nThis\'ll just take a moment.\n');
+
+print.subtitle('Installing dependencies...');
+exec('npm install --save-dev dce-selenium mocha');
+
+// Add selenium script
+print.subtitle('Updating test script in package.json')
+const newPackageJSON = getPackageJSON();
+const scripts = newPackageJSON.scripts || {};
+scripts.selenium = 'node ./node_modules/dce-selenium/run';
+newPackageJSON.scripts = scripts;
+writePackageJSON(newPackageJSON);
+console.log('package.json updated\n');
+
+// Create test folder
+print.subtitle('Creating test/ folder');
+exec('mkdir -p test');
+console.log('test/ folder created\n');
+
+// Add eslint rules
+print.subtitle('Adding ESLint rules for testing');
+copyTo(
+  path.join(__dirname, '.eslintrc.json'),
+  path.join(currDir, 'test', '.eslintrc.json')
+);
+console.log('/test/.eslintrc.json file create\n');
+
+// Update .gitignore
+stepTitle('Updating .gitignore');
+const gitignoreFilename = path.join(currDir, 'test', '.gitignore');
+let gitignore = (
+  fs.existsSync(gitignoreFilename)
+    ? fs.readFileSync(gitignoreFilename, 'utf-8')
+    : ''
+);
+gitignore += (gitignore.length > 0 ? '\n' : '');
+gitignore += '# Ignore snapshots\nsnapshots/';
+fs.writeFileSync(gitignoreFilename, gitignore, 'utf-8');
+console.log('.gitignore updated');
+
+console.log('\n');
+
+print.title('Done!');
+
+console.log('\nMake sure your code editor is set up to enforce ESLint rules.');
---
+++ init.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+/**
+ * This code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+var nodeVersion = process.versions.node;
+var parts = nodeVersion.split('.');
+var majorVersion = parts[0];
+
+if (majorVersion < 8) {
+  console.error('Oops! You are running node version ' + majorVersion + '.');
+  console.log('This initializer requires Node 8 or higher. Please update your version of Node.');
+  return;
+}
+
+require('./index.js');
